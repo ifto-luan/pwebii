de5256a794730d545bd26ba8f44ccf558c8e8aa9
ifto-luan <luan.souza8@estudante.ifto.edu.br>
2025-02-23 16:19:17 -0300

    agora sim

--- src/main/java/com/pwebii/jpa_heranca/controller/ForgotPasswordController.java
+++ src/main/java/com/pwebii/jpa_heranca/controller/ForgotPasswordController.java
@@ -5,7 +5,6 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
--- src/main/java/com/pwebii/jpa_heranca/model/entity/Order.java
+++ src/main/java/com/pwebii/jpa_heranca/model/entity/Order.java
@@ -19,7 +19,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
